15a89f94a503fa05844d6144a31be8cf35ab2e42
Плетнев Никита Сергеевич <45274552+NikitaPletnev@users.noreply.github.com>
2019-04-05 00:26:48 +0300

    Update main.js
    Добавлена более тщательная проверка входных данных и устранена проблема дублирования имен

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 "use strict";
 (function() {
+  /*Создание счетчика для операции с массивами*/
+  let i = 0;
+  /*Создание массива , в который будет записываться входная информация*/
+  let arr = [];
   /*Объявление основного обьекта */
   let object = {
     /*Функция-конструктор данных о пользователе*/
@@ -25,13 +29,22 @@
   while(object.promptName !== null){
     /*Ввод информации пользователем*/
     object.promptName = prompt('Введите имя и фамилию пользователя,пожалуйста!');
+    /*Внесение данных в массив*/
+    arr.push(object.promptName);
+    /*Отбор уникальных данных и помещение их в новый массив*/
+    let unique = arr.filter((v, i, a) => a.indexOf(v) === i); 
+   /* Проверка на число*/
+    let CheckNum = isNaN(parseInt(object.promptName));
     /*Проверка входных данных*/
-    if(object.promptName !== null && (object.promptName.trim() !== '' && object.promptName.split(' ').length === 2)){
+    if(unique[i] !== undefined && CheckNum === true && object.promptName !== null && (object.promptName.trim() !== '' && object.promptName.split(' ').length === 2)){
+    
       /*Добавление данных*/
       UserList.add(new object.User({
         firstName: object.promptName.trim().split(' ')[0],
         lastName: object.promptName.trim().split(' ')[1],
       }));
+      /*Увеличение счетчика*/
+      i++;
     }
   }
   /*Вывод данных в консоль*/
